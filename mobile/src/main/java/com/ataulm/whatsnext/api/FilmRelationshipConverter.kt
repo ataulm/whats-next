e6d884866eea99ea5930dba91467681141c408bb
Ataul Munim <hello@ataulm.com>
2017-08-02 22:22:47 +0100

    Remove unnecessary default ctor marker from FilmRelationshipConverter

--- mobile/src/main/java/com/ataulm/whatsnext/api/FilmRelationshipConverter.kt
+++ mobile/src/main/java/com/ataulm/whatsnext/api/FilmRelationshipConverter.kt
@@ -2,7 +2,7 @@ package com.ataulm.whatsnext.api
 
 import com.ataulm.whatsnext.FilmRelationship
 
-internal class FilmRelationshipConverter() {
+internal class FilmRelationshipConverter {
 
     fun convert(apiFilmRelationship: ApiFilmRelationship): FilmRelationship {
         return FilmRelationship(apiFilmRelationship.watched, apiFilmRelationship.liked, apiFilmRelationship.inWatchlist, apiFilmRelationship.rating);
