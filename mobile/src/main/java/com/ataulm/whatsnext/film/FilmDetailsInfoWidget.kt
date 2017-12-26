80388aca08c76a72f4f645acc9bb5c2f4c68d6ae
Ataul Munim <hello@ataulm.com>
2017-12-26 21:09:59 +0000

    Replace FilmDetailsInfoWidget nested layouts with ConstraintLayout

--- mobile/src/main/java/com/ataulm/whatsnext/film/FilmDetailsInfoWidget.kt
+++ mobile/src/main/java/com/ataulm/whatsnext/film/FilmDetailsInfoWidget.kt
@@ -1,10 +1,10 @@
 package com.ataulm.whatsnext.film
 
 import android.content.Context
+import android.support.constraint.ConstraintLayout
 import android.util.AttributeSet
 import android.view.View
 import android.widget.ImageView
-import android.widget.LinearLayout
 import android.widget.TextView
 import butterknife.BindView
 import butterknife.ButterKnife
@@ -12,18 +12,14 @@ import com.ataulm.whatsnext.FilmSummary
 import com.ataulm.whatsnext.R
 import com.bumptech.glide.Glide
 
-class FilmDetailsInfoWidget constructor(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
+class FilmDetailsInfoWidget constructor(context: Context, attrs: AttributeSet) : ConstraintLayout(context, attrs) {
 
     @BindView(R.id.film_details_info_text_title) lateinit var titleTextView: TextView
-    @BindView(R.id.film_details_info_text_release_year) lateinit var releaseYearTextView: TextView
+    @BindView(R.id.film_details_info_text_release_year_director) lateinit var releaseYearDirectorTextView: TextView
     @BindView(R.id.film_details_info_text_runtime) lateinit var runtimeTextView: TextView
     @BindView(R.id.film_details_info_image_poster) lateinit var posterImageView: ImageView
     @BindView(R.id.film_details_info_text_tagline) lateinit var taglineTextView: TextView
     @BindView(R.id.film_details_info_text_description) lateinit var descriptionTextView: TextView
-
-    init {
-        orientation = VERTICAL
-    }
 
     override fun onFinishInflate() {
         super.onFinishInflate()
@@ -33,7 +29,7 @@ class FilmDetailsInfoWidget constructor(context: Context, attrs: AttributeSet) :
 
     fun bind(filmSummary: FilmSummary) {
         titleTextView.text = filmSummary.name
-        releaseYearTextView.text = filmSummary.year
+        releaseYearDirectorTextView.text = filmSummary.year + " | Director" // TODO get real director and use resource string
         runtimeTextView.text = filmSummary.runtimeMinutes.toString()
         taglineTextView.text = filmSummary.tagline
         descriptionTextView.text = filmSummary.description
