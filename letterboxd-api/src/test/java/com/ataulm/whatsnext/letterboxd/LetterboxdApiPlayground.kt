cb5a6a27e6bbb35c74a3b47746db24cf2262beb4
Ataul Munim <hello@ataulm.com>
2018-04-15 13:41:35 +0100

    Add playground for testing api calls

---
+++ letterboxd-api/src/test/java/com/ataulm/whatsnext/letterboxd/LetterboxdApiPlayground.kt
@@ -0,0 +1,49 @@
+package com.ataulm.whatsnext.letterboxd
+
+import com.ataulm.support.Clock2
+import org.junit.Test
+import java.io.File
+import java.io.FileReader
+import java.util.*
+
+class LetterboxdApiPlayground {
+
+    @Test
+    fun film() {
+        val response = signedInApi().film("1Wow").blockingFirst()
+        print(response)
+    }
+
+    private fun signedInApi(): LetterboxdApi {
+        val tokenStore = object : TokenStore {
+
+            var token: AccessToken? = null
+
+            override fun store(token: AccessToken) {
+                this.token = token
+            }
+
+            override fun clear() {
+                token = null
+            }
+
+            override fun token(): AccessToken? {
+                return token
+            }
+        }
+
+        val properties = Properties()
+        properties.load(FileReader(File("letterboxd.properties")))
+
+        val api = LetterboxdApiFactory(
+                tokenStore,
+                properties.getProperty("apiKey"),
+                properties.getProperty("apiSecret"),
+                Clock2()
+        ).create()
+
+        val accessToken = api.accessToken(properties.getProperty("username"), properties.getProperty("password")).blockingFirst()
+        tokenStore.store(accessToken)
+        return api
+    }
+}
