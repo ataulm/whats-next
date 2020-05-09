6c95e4873bf228ab7c2e472e8bbd1a27d0c71b0e
Ataul Munim <hello@ataulm.com>
2020-05-09 12:23:50 +0100

    Add "fetch new access token" error handler

--- app/src/main/java/com/ataulm/whatsnext/WhatsNextService.java
+++ app/src/main/java/com/ataulm/whatsnext/WhatsNextService.java
@@ -11,19 +11,27 @@ import com.ataulm.whatsnext.api.LetterboxdApi;
 import java.util.ArrayList;
 import java.util.List;
 
+import io.reactivex.Completable;
+import io.reactivex.CompletableSource;
 import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.ObservableTransformer;
 import io.reactivex.Single;
+import io.reactivex.functions.Action;
 import io.reactivex.functions.BiFunction;
 import io.reactivex.functions.Function;
+import retrofit2.HttpException;
 
 public class WhatsNextService {
 
     private final LetterboxdApi letterboxdApi;
+    private final TokensStore tokensStore;
     private final FilmSummaryConverter filmSummaryConverter;
     private final FilmRelationshipConverter filmRelationshipConverter;
 
-    public WhatsNextService(LetterboxdApi letterboxdApi, FilmSummaryConverter filmSummaryConverter, FilmRelationshipConverter filmRelationshipConverter) {
+    public WhatsNextService(LetterboxdApi letterboxdApi, TokensStore tokensStore, FilmSummaryConverter filmSummaryConverter, FilmRelationshipConverter filmRelationshipConverter) {
         this.letterboxdApi = letterboxdApi;
+        this.tokensStore = tokensStore;
         this.filmSummaryConverter = filmSummaryConverter;
         this.filmRelationshipConverter = filmRelationshipConverter;
     }
@@ -79,6 +87,50 @@ public class WhatsNextService {
                         );
                     }
                 }
-        ).toObservable();
+        )
+                .toObservable()
+                .compose(this.<Film>refreshAccessTokenIfNecessary());
     }
+
+    private <T> ObservableTransformer<T, T> refreshAccessTokenIfNecessary() {
+        return new ObservableTransformer<T, T>() {
+            @Override
+            public ObservableSource<T> apply(final Observable<T> upstream) {
+                return upstream.onErrorResumeNext(new Function<Throwable, ObservableSource<T>>() {
+                    @Override
+                    public ObservableSource<T> apply(Throwable throwable) {
+                        if (throwable instanceof HttpException && ((HttpException) throwable).code() == 401) {
+                            return refreshAccessToken().andThen(upstream);
+                        } else {
+                            return Observable.error(throwable);
+                        }
+                    }
+                });
+            }
+        };
+    }
+
+    private Completable refreshAccessToken() {
+        Token token = tokensStore.getToken();
+        if (token == null) {
+            return Completable.error(new UserRequiresSignInException());
+        }
+        String refreshToken = tokensStore.getToken().getRefreshToken();
+        return letterboxdApi.refreshAuthToken(refreshToken, "refresh_token")
+                .map(toToken())
+                .flatMapCompletable(new Function<Token, CompletableSource>() {
+                    @Override
+                    public CompletableSource apply(final Token token) {
+                        return Completable.fromAction(new Action() {
+                            @Override
+                            public void run() {
+                                tokensStore.store(token);
+                            }
+                        });
+                    }
+                });
+    }
+}
+
+class UserRequiresSignInException extends RuntimeException {
 }
