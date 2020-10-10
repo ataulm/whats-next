d2f0973a7d9a6ace61eda4b37cee96489afd7844
Ataul Munim <hello@ataulm.com>
2020-10-10 13:46:23 +0100

    Remove unused signin screen

--- app/src/main/java/com/ataulm/whatsnext/Navigator.java
+++ app/src/main/java/com/ataulm/whatsnext/Navigator.java
@@ -3,9 +3,7 @@ package com.ataulm.whatsnext;
 import android.app.Activity;
 import android.content.Intent;
 
-import com.ataulm.whatsnext.account.SignInActivity;
 import com.ataulm.whatsnext.film.FilmActivity;
-import com.ataulm.whatsnext.search.SearchActivity;
 import com.ataulm.whatsnext.watchlist.WatchListActivity;
 
 public class Navigator {
@@ -19,11 +17,6 @@ public class Navigator {
     public void navigateToFilm(FilmSummary filmSummary) {
         Intent intent = new Intent(activity, FilmActivity.class);
         intent.putExtra(FilmActivity.EXTRA_FILM_SUMMARY, filmSummary);
-        activity.startActivity(intent);
-    }
-
-    public void navigateToSignIn() {
-        Intent intent = new Intent(activity, SignInActivity.class);
         activity.startActivity(intent);
     }
 
