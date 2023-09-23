a22295fc82876ed39c025ac11a61bbf6ef91b580
Ataul Munim <hello@ataulm.com>
2023-09-23 21:44:07 +0100

    Remove buildConfig plugin
    - since I'm using an android lib plugin now which has buildConfig already

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,6 @@ plugins {
     @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
     alias(libs.plugins.android) apply (false)
     @Suppress("DSL_SCOPE_VIOLATION")
-    alias(libs.plugins.build.config) apply false
-    @Suppress("DSL_SCOPE_VIOLATION")
     alias(libs.plugins.kotlin.android) apply (false)
     @Suppress("DSL_SCOPE_VIOLATION")
     alias(libs.plugins.kotlin.parcelize) apply (false)
